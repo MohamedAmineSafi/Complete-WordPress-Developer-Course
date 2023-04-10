989ecf205e7408f019e48c8077c1541ae66697a1
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-10 12:26:21 +0100

    Loading Additional Tags

--- app/public/wp-content/themes/udemy/index.php
+++ app/public/wp-content/themes/udemy/index.php
@@ -2,14 +2,14 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
 </head>
 
 <body>
     <p>Hello world</p>
+
+    <?php wp_footer(); ?>
 </body>
 
 </html>
