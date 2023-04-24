458c44b322313eec1d3516df3cfc4e54247a3233
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-24 11:23:34 +0100

    Dynamic Content

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const div = React.createElement(
-  'div', null, [
-    React.createElement('h1', null, "hi"),
-    React.createElement('h2', null, 'Bye')
-  ]
-);
+function Page() {
+  return React.createElement(
+    'div', null, [
+      React.createElement('h1', null, `Hi ${Date().toLocaleString()}`),
+      React.createElement('h2', null, 'Bye')
+    ]
+  );
+}
 
 const rootEl = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootEl);
 
-root.render(div);
+setInterval(function () {
+  root.render(Page());
+}, 1000);
