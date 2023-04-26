64e1ffebe16bf13786275582b8607a3180123265
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-26 10:46:16 +0100

    Enqueuing Styles for Blocks

--- app/public/wp-content/plugins/udemy-plus/build/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8079085ce0a61cf19d82');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3fab4950c9a722f0bdc8');
