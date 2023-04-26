3c16bb31e2e280e1c14449464799d4426853f6e2
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-26 08:44:07 +0100

    Adding a Custom Block

--- app/public/wp-content/plugins/udemy-plus/build/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c113ef3ea88998350041');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '8eec7663ee9491a46edd');
