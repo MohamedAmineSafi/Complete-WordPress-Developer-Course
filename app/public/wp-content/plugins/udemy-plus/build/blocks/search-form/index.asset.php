6dea05646800ff7ec02b79799ff4a1767235aac5
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-27 15:34:13 +0100

    Adding the Template

--- app/public/wp-content/plugins/udemy-plus/build/blocks/search-form/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => 'bad0fc350aedb261a84e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '082da424a1e7f7a3316b');
