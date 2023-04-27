c04526a583311c34318140a76b27ec921cab026d
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-27 14:08:51 +0100

    Adding a Color Palette

--- app/public/wp-content/plugins/udemy-plus/build/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c82e05a3668040e72565');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f162d0441c8942d74fd2');
