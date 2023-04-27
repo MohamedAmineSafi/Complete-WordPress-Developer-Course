2f38a918ed2706a5f97b88bacba45870348654c6
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-27 14:21:39 +0100

    Attribute Sources

--- app/public/wp-content/plugins/udemy-plus/build/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a10fc32c36ed7871e81c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4d6cefe6ab037872259d');
