7ce00e3be17959c20317b3eeb713c9f803705352
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-27 16:22:12 +0100

    Exercise: Applying Colors

--- app/public/wp-content/plugins/udemy-plus/build/blocks/search-form/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '4b3e5310143ab0df657c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a7d39aade0973b5196e3');
