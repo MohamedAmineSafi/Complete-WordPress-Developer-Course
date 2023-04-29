23d9ee9c7513b60c2eb4da33dd98a206247627dd
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-04-29 09:44:45 +0100

    Exercise: Adding the RichText Component

--- app/public/wp-content/plugins/udemy-plus/build/blocks/page-header/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/page-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '9c84d1329b78e90e72e9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e00427f350f96bc6f8b4');
--- app/public/wp-content/plugins/udemy-plus/src/blocks/page-header/index.js
+++ app/public/wp-content/plugins/udemy-plus/src/blocks/page-header/index.js
@@ -1,21 +1,31 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { 
-  useBlockProps
+  useBlockProps,
+  RichText
 } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n'
 import icons from '../../icons.js'
 import './main.css'
 
 registerBlockType('udemy-plus/page-header', {
   icon: icons.primary,
-	edit() {
-    const blockProps = useBlockProps();
+	edit({attributes, setAttributes}) {
+        const {content} = attributes;
+        const blockProps = useBlockProps();
 
-    return (
-      <>
-        <div { ...blockProps }>
-          Page Header
-        </div>
-      </>
-    );
+        return (
+        <>
+            <div {...blockProps}>
+                <div className="inner-page-header">
+                    <RichText
+                        tagName='h1'
+                        placeholder={__('Heading', 'udemy-plus')}
+                        value={content}
+                        onChange={(content) => {setAttributes({content})}}
+                    />
+                </div>
+            </div>
+        </>
+        );
   }
 });
