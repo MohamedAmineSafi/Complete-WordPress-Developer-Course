ee913ada83d8828915d2277b67582d8ce5c20b1d
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-02 09:40:01 +0100

    Finishing the Header Tools Block

--- app/public/wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
+++ app/public/wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
@@ -1,6 +1,28 @@
 <?php
 
-function up_header_tools_render_cb()
+function up_header_tools_render_cb($atts)
 {
+    ob_start();
+    ?>
 
+    <?php
+    if ($atts['showAuth']) {
+        ?>
+        <div class="wp-block-udemy-plus-header-tools">
+            <a class="signin-link open-modal" href="#">
+                <div class="signin-icon">
+                    <i class="bi bi-person-circle"></i>
+                </div>
+                <div class="signin-text">
+                    <small>Hello, Sign in</small>
+                    My Account
+                </div>
+            </a>
+        </div>
+        <?php
+    }
+
+    $output = ob_get_contents();
+    ob_get_clean();
+    return $output;
 }
