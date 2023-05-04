269c4122e44a721ad5acc8aacb8a9e4bdce8373d
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-04 14:38:29 +0100

    Validating the Request Parameters

--- app/public/wp-content/plugins/udemy-plus/includes/rest-api/signup.php
+++ app/public/wp-content/plugins/udemy-plus/includes/rest-api/signup.php
@@ -1,10 +1,21 @@
 <?php
 
-function up_rest_api_signup_handler()
+function up_rest_api_signup_handler($req)
 {
     $response = ['status' => 1];
+    $params = $req->get_json_params();
 
-
+    if (
+        !isset($params['email'], $params['username'], $params['password'])
+        ||
+        empty($params['email'])
+        ||
+        empty($params['username'])
+        ||
+        empty($params['password'])
+    ) {
+        return $response;
+    }
 
     $response = ['status' => 2];
     return $response;
