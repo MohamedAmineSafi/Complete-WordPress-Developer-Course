54c84880a1f2dfb058e3adb1fe3b2cc87b98b621
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-06 11:07:25 +0100

    Flushing Rewrite Rules

--- app/public/wp-content/plugins/udemy-plus/includes/activate.php
+++ app/public/wp-content/plugins/udemy-plus/includes/activate.php
@@ -5,4 +5,7 @@ function up_activate_plugin()
     if (version_compare(get_bloginfo('version'), '5.9', '<')) {
         wp_die(__('You must update WP', 'udemy-plus'));
     }
+
+    up_recipe_post_type();
+    flush_rewrite_rules();
 }
