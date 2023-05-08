71cfe7ab4458588a51badecb68c181ae5d2b999c
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-08 21:03:30 +0100

    Creating the Recipe Summary Block

---
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5a2f81264d62f7dd0797');
--- app/public/wp-content/plugins/udemy-plus/includes/register-blocks.php
+++ app/public/wp-content/plugins/udemy-plus/includes/register-blocks.php
@@ -27,6 +27,12 @@ function up_register_blocks()
             'options' => [
                 'render_callback' => 'up_auth_modal_render_cb'
             ]
+        ],
+        [
+            'name' => 'recipe-summary',
+            'options' => [
+                'render_callback' => 'up_recipe_summary_render_cb'
+            ]
         ]
     ];
 
