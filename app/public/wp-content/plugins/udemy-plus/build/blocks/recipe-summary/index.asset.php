cf60fd4005563cf35e5905a03c495a658ea0366e
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-10 09:16:00 +0100

    Waiting for Data to be Resolved

--- app/public/wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9361159f462654929637');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '63e8d94295e32a5047d3');
