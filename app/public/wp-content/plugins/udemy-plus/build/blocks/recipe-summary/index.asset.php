1a9df3048a6d8f6b0450b02a7d43a1b3cd91a9cd
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-10 09:34:19 +0100

    Grabbing Post Metadata

--- app/public/wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '63e8d94295e32a5047d3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd07176085640da9be510');
