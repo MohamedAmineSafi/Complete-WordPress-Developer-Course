aa3afc3625f04c39b5fd115ff5c593266cc6a6d3
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-10 09:06:55 +0100

    Mapping the Cuisines

--- app/public/wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '06ae005c2860d2ca1070');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9361159f462654929637');
