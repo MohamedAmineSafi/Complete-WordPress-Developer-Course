ba5f6dd2e873319a638249ef8135e8add66f759c
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-12 10:35:09 +0100

    (Bug Fix)

--- app/public/wp-content/plugins/example.php
+++ app/public/wp-content/plugins/example.php
@@ -5,6 +5,5 @@ add_action('recipe_rated', function ($data) {
     $authorEmail = get_the_author_meta('user_email', $post->post_author);
     $subject = "Your recipe has received a new rating";
     $message = "Your recipe {$post->post_title} has received a rating of {$data['rating']}";
+    wp_mail($authorEmail, $subject, $message);
 });
-
-wp_mail($authorEmail, $subject, $message);
