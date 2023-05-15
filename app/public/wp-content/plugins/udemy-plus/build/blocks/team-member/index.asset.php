42ac3afa2f2c4cef10feca5cff0d51e114587f82
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-15 18:49:12 +0100

    Quickly Refactoring the Team Member Block

--- app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '77261a0874cf818fafe1');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '64ed9a5c6d7efff9b7a7');
