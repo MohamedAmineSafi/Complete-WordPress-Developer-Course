3533898c5fe092f9078132f96a94ef44d60fbbfa
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-15 17:22:32 +0100

    Adding Columns

--- app/public/wp-content/plugins/udemy-plus/build/blocks/team-members-group/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/team-members-group/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '800d7833a578792064f1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd7d6e97ddc9d700c6f9f');
