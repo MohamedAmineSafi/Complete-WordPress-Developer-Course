988d903279bdfe98dec239572a9ed58380477553
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-15 18:27:19 +0100

    Handling Blob Images

--- app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fcdfac28298691fb3368');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f266fcc2f56cabe224b6');
