a6232aee33a77c6cbf623e9e746d8a2951721e3d
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-15 18:42:53 +0100

    Fixing Blob Memory Leaks

--- app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f818cb717dc0f1337312');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '77261a0874cf818fafe1');
