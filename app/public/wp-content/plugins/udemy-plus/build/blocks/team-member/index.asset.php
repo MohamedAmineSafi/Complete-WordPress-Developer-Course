bc9014d04349dcb0e8c5b63f0a53e86c19f24052
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-15 18:30:37 +0100

    Exercise: Adding a Spinner

--- app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f266fcc2f56cabe224b6');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7c0bb67f1fcf8d0bf871');
