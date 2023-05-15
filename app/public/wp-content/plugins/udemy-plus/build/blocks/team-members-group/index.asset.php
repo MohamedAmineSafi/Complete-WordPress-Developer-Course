e6169b77f38351d7768263626d8b85cba51ebc89
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-15 17:18:07 +0100

    Inner Blocks Template Options

--- app/public/wp-content/plugins/udemy-plus/build/blocks/team-members-group/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/team-members-group/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '63ad6c57ab69bf6dc83d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '800d7833a578792064f1');
