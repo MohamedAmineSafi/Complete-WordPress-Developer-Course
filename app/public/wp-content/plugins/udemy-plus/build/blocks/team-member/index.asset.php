45ebb55f9ecfcc3d583722532ad4b9d2daefc7ef
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-15 18:12:23 +0100

    Handling Selections

--- app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
+++ app/public/wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e566896b4dfb1a98f1e9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '59d59dfc8bcc32a01fdf');
